320089f5eb6d3936a163bc6e625f30c265a5cc29
Adam LaPlante <adam.laplante@emc.com>
2016-01-27 13:29:26 -0700

    first commit playing with google proto buf

---
+++ src/main/java/com/adam/learning/Application.java
@@ -0,0 +1,12 @@
+package com.adam.learning;
+
+import com.adam.protos.AddressBookProtos;
+
+/**
+ * Created by alaplante on 1/27/16.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Running application");
+    }
+}
